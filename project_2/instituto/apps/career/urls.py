f28ea5018d1050227b8e81d63249a5e163a27639
Juan Ignacio Bonini <juanignaciobonini@hotmail.com>
2025-05-20 19:43:02 -0300

    fix django

--- project_2/instituto/apps/career/urls.py
+++ project_2/instituto/apps/career/urls.py
@@ -1,4 +1,3 @@
-
 from django.urls import path
 
 from apps.career.views import career_list
--- project_2/instituto/apps/subject/admin.py
+++ project_2/instituto/apps/subject/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.subject.models import Subject
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    list_filter = ('name',)
+    ordering = ('name',)
+    list_per_page = 10
